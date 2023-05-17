de6848af83fedd3b548cfbd29cd43a98b35fb115
Cameron <103131615+Cameron-IPFSPodcasting@users.noreply.github.com>
2023-05-16 21:34:56 -0400

    Add files via upload

--- scripts/procedures/getConfig.ts
+++ scripts/procedures/getConfig.ts
@@ -1,35 +1,6 @@
 import { compat, types as T } from "../deps.ts";
 
 export const getConfig: T.ExpectedExports.getConfig = compat.getConfig({
-/*
-  "ipfs-id": {
-    "name": "gc IPFS ID",
-    "description": "gc Your IPFS Node ID on the IPFS Network.",
-    "type": "pointer",
-    "subtype": "package",
-    "package-id": "ipfs-podcasting",
-    "target": "ipfs-id",
-    "interface": "main"
-  },
-  "peer-count": {
-    "name": "gc Peer Count",
-    "description": "gc Number of IPFS peers connected to your node. More nodes mean [better performance](https://ipfspodcasting.net/Help/Network).",
-    "type": "pointer",
-    "subtype": "package",
-    "package-id": "ipfs-podcasting",
-    "target": "peer-count",
-    "interface": "main"
-  },
-  "disk-usage": {
-    "name": "gc Disk Usage",
-    "description": "gc Disk used by IPFS. This is the size of your IPFS Datastore which may contain files not used for IPFS Podcasting. To clean up your datastore, select `Clean Up` from the `Actions` menu.",
-    "type": "pointer",
-    "subtype": "package",
-    "package-id": "ipfs-podcasting",
-    "target": "disk-usage",
-    "interface": "main"
-  },
-*/
   "email-address": {
     "type": "string",
     "name": "E-Mail (optional)",
